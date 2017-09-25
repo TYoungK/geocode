19865cc709cb5da65cc9e13f50d94784d3f8329c
D7703_29 <d7703_29@D7703-29ui-iMac.local>
2017-09-25 15:35:03 +0900

    geocoded

--- geocording/ViewController.swift
+++ geocording/ViewController.swift
@@ -6,20 +6,86 @@
 //  Copyright © 2017년 D7703_29. All rights reserved.
 //
 
-import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var map: MKMapView!
+    
+    
+    //var annotations = [MKPointAnnotation]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        zoomToRegion()
+        
+        /////////////////////////////
+        
+        let path = Bundle.main.path(forResource: "pinmark", ofType: "plist")
+        print("path = \(String(describing: path))")
+        
+        let contents = NSArray(contentsOfFile: path!)
+        print("path = \(String(describing: contents))")
+        
+        var annotations = [MKPointAnnotation]()
+        
+        map.delegate = self
+        
+        // optional binding
+        if let myItems = contents {
+            // Dictionary Array에서 값 뽑기
+            for item in myItems {
+                let address = (item as AnyObject).value(forKey: "address")
+                let title = (item as AnyObject).value(forKey: "title")
+                
+                let geoCoder = CLGeocoder()
+                
+                geoCoder.geocodeAddressString(address as! String, completionHandler: { placemarks, error in
+                    
+                    if error != nil {
+                        print(error!)
+                        return
+                    }
+                    
+                    if let myPlacemarks = placemarks {
+                        let myPlacemark = myPlacemarks[0]
+                        
+                        print("placemark[0] = \(String(describing: myPlacemark.name))")
+                        
+                        let annotation = MKPointAnnotation()
+                        annotation.title = title as? String
+                        annotation.subtitle = address as? String
+                        
+                        if let myLocation = myPlacemark.location {
+                            annotation.coordinate = myLocation.coordinate
+                            annotations.append(annotation)
+                        }
+                        
+                    }
+                    print("annotations = \(annotations)")
+                    self.map.showAnnotations(annotations, animated: true)
+                    self.map.addAnnotations(annotations)
+                    
+                })
+            }
+        } else {
+            print("contents의 값은 nil")
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func zoomToRegion() {
+        
+        //  DIT 위치정보 35.166197, 129.072594
+        let center = CLLocationCoordinate2DMake(35.166197, 129.072594)
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegionMake(center, span)
+        
+        map.setRegion(region, animated: true)
+        
     }
-
 
 }
 
